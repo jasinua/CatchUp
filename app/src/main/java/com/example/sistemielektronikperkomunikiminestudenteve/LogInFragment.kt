1c6754b1bc43e5428809e4bf53b526775cf0d760
Marijanito <marjankolaj14@gmail.com>
2023-11-20 15:40:32 +0100

    E kam ndreq fragment start, dhe do sene ne Main Activity

--- app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/LogInFragment.kt
+++ app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/LogInFragment.kt
@@ -5,17 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sistemielektronikperkomunikiminestudenteve.databinding.FragmentLogInBinding
 
 class LogInFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding  = FragmentLogInBinding.inflate(layoutInflater)
 
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/MainActivity.kt
+++ app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/MainActivity.kt
@@ -10,6 +10,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.sistemielektronikperkomunikiminestudenteve.databinding.ActivityMainBinding
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.Firebase
 import com.google.firebase.auth.AuthResult
@@ -22,21 +26,28 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.google.firebase.database.getValue
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_log_in)
 
-        val login = findViewById<Button>(R.id.logInButton)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val database = Firebase.database
-        var auth = FirebaseAuth.getInstance();
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        val emailText = findViewById<EditText>(R.id.idInput)
-        val passwordText = findViewById<EditText>(R.id.passwordInput)
-
-
+        setupActionBarWithNavController(navController)
 
 
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
