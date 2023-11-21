95702a094c2b33e0993d6d04a400ac68f9430ad3
jasinua <115897385+jasinua@users.noreply.github.com>
2023-11-21 22:49:59 +0100

    e bana commit mas log in

--- app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/LogInFragment.kt
+++ app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/LogInFragment.kt
@@ -1,5 +1,6 @@
 package com.example.sistemielektronikperkomunikiminestudenteve
 
+import android.content.ContentValues
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,16 +11,40 @@ import com.example.sistemielektronikperkomunikiminestudenteve.databinding.Fragme
 class LogInFragment : Fragment() {
 
 
+    lateinit var dbHelper:DBHandler
+
     private lateinit var binding: FragmentLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding  = FragmentLogInBinding.inflate(layoutInflater)
+
 
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+
+        binding  = FragmentLogInBinding.inflate(layoutInflater)
+
+                dbHelper = DBHandler(this.context)
+//
+//        val db = dbHelper.writableDatabase
+
+
+        var inputEmail = binding.idInput
+        var inputPassword = binding.passwordInput
+        var button = binding.logInButton
+
+        var text = binding.logInText
+
+        button.setOnClickListener{
+
+            dbHelper.addNewStudent(inputEmail.text.toString(), inputPassword.text.toString())
+
+
+        }
+
         // Inflate the layout for this fragment
         return binding.root
     }
