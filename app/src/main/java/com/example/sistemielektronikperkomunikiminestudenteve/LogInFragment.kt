a80f4fbf3c9123b9d0de8a5bab07a3d64fe8162a
Marijanito <marjankolaj14@gmail.com>
2023-11-22 01:24:35 +0100

    log in i lidhum me firebase qe eshte ne emailin e databazes ( ja kam hek DBHandler edhe kto bindings)

--- app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/LogInFragment.kt
+++ app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/LogInFragment.kt
@@ -6,14 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
 import com.example.sistemielektronikperkomunikiminestudenteve.databinding.FragmentLogInBinding
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
 
 class LogInFragment : Fragment() {
 
 
-    lateinit var dbHelper:DBHandler
 
-    private lateinit var binding: FragmentLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,32 +27,7 @@ class LogInFragment : Fragment() {
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
 
 
-        binding  = FragmentLogInBinding.inflate(layoutInflater)
-
-                dbHelper = DBHandler(this.context)
-//
-//        val db = dbHelper.writableDatabase
-
-
-        var inputEmail = binding.idInput
-        var inputPassword = binding.passwordInput
-        var button = binding.logInButton
-
-        var text = binding.logInText
-
-        button.setOnClickListener{
-
-            dbHelper.addNewStudent(inputEmail.text.toString(), inputPassword.text.toString())
-
-
-        }
-
-        // Inflate the layout for this fragment
-        return binding.root
-    }
 
 }
--- app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/MainActivity.kt
+++ app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/MainActivity.kt
@@ -1,27 +1,18 @@
 package com.example.sistemielektronikperkomunikiminestudenteve
 
-import android.content.ContentValues.TAG
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
 import android.util.Log
-import android.util.Patterns
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.sistemielektronikperkomunikiminestudenteve.databinding.ActivityMainBinding
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.Firebase
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.google.firebase.database.getValue
@@ -29,26 +20,72 @@ import com.google.firebase.database.getValue
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-//        setupActionBarWithNavController(navController)
+        setContentView(R.layout.fragment_log_in)
 
 
+        val database = Firebase.database
+        val ref = database.getReference("USERS")
 
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.host_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        val login = findViewById<Button>(R.id.logInButton)
+        val loginEmail = findViewById<EditText>(R.id.idInput)
+        val loginPass = findViewById<EditText>(R.id.passwordInput)
+
+        login.setOnClickListener(){
+
+        var exists = false
+            ref.addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val count = dataSnapshot.childrenCount
+
+                    for(dataSnapshot1 : DataSnapshot in dataSnapshot.getChildren()){
+
+                        var email = dataSnapshot1.child("EMAIL").getValue().toString()
+                        var pass = dataSnapshot1.child("PASS").getValue().toString()
+
+                        if(!exists) {
+
+                            if (email.equals(loginEmail.text.toString()) && pass.equals(loginPass.text.toString())) {
+                                exists = true
+                                setContentView(R.layout.fragment_home_page)
+                            }
+                        }
+
+
+                    }
+
+                    for(dataSnapshot1 : DataSnapshot in dataSnapshot.getChildren()){
+
+                        var email = dataSnapshot1.child("EMAIL").getValue().toString()
+                        var pass = dataSnapshot1.child("PASS").getValue().toString()
+
+                        if(!exists){
+                                ref.child(count.toString()).child("EMAIL")
+                                    .setValue(loginEmail.text.toString())
+                                ref.child(count.toString()).child("PASS")
+                                    .setValue(loginPass.text.toString())
+
+                        }
+
+                    }
+
+
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
+
+
+        }
+
+
     }
 
 }
