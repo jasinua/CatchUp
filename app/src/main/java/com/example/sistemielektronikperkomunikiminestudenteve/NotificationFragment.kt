a7756288e69ce6fa3c20344452be501d9cd1e884
Marijanito <marjankolaj14@gmail.com>
2023-12-14 21:45:53 +0100

    i qita qato te gezeartes

--- app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/NotificationFragment.kt
+++ app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/NotificationFragment.kt
@@ -5,8 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sistemielektronikperkomunikiminestudenteve.databinding.FragmentNotificationBinding
 
-class NotificationFragment : Fragment(R.layout.fragment_focused_post) {
-
+class NotificationFragment : Fragment(R.layout.fragment_notification) {
 
 }
