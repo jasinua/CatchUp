67516ed24ce61ef4e6cc23efc2a52a1660164d4d
Gezearta <gezeartaabazi11@gmail.com>
2023-12-21 22:52:48 +0100

    new fragment for changing the password

--- app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/Fragments/ProfileFragment.kt
@@ -26,12 +26,18 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         val id = view.findViewById<TextView>(R.id.profileId)
         val drejtimi = view.findViewById<TextView>(R.id.profileDrejtim)
         val email = view.findViewById<TextView>(R.id.profileEmail)
+        val passwordchange=view.findViewById<Button>(R.id.changepassword)
 
         val logout = view.findViewById<Button>(R.id.logOutButton)
 
         id.text=idInfo
 
         val dbRef = FirebaseDatabase.getInstance().getReference("USERS")
+
+        passwordchange.setOnClickListener {
+
+        }
+
 
         dbRef.addValueEventListener(object:ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
