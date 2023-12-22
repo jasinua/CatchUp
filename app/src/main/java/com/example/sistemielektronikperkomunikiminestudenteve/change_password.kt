5ac3e683e423291a60ec7dc51489ab645696c106
Marijanito <marjankolaj14@gmail.com>
2023-12-22 21:58:42 +0100

    same shit veq i nrreqa errorat qe i pat ba commit gazeta

--- app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/change_password.kt
+++ app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/change_password.kt
@@ -1,26 +1,8 @@
 package com.example.sistemielektronikperkomunikiminestudenteve
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 class change_password : Fragment() {
 
 
-
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_example, container, false)
-        }
-    }
-
 }
+
