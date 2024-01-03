2315cebe0e66f2db4ec3c69fff51085acd0d6206
Marijanito <marjankolaj14@gmail.com>
2024-01-03 23:42:23 +0100

    downloading files (duhesh me prek back on afate documents qtash per qtash per me ta ba download the specific file qe e kam qit mu ba) (mos e ban spam prite nja 3 sekonda)

--- app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/Fragments/Docs/openAfateDocument.kt
+++ app/src/main/java/com/example/sistemielektronikperkomunikiminestudenteve/Fragments/Docs/openAfateDocument.kt
@@ -1,47 +1,73 @@
 package com.example.sistemielektronikperkomunikiminestudenteve.Fragments.Docs
 
+import android.app.DownloadManager
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment.DIRECTORY_DOWNLOADS
 import android.view.View
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.sistemielektronikperkomunikiminestudenteve.Fragments.DocumentsFragment
 import com.example.sistemielektronikperkomunikiminestudenteve.MainActivity
 import com.example.sistemielektronikperkomunikiminestudenteve.R
 import com.google.firebase.storage.FirebaseStorage
-import java.io.File
+import com.google.firebase.storage.StorageReference
+
 
 class openAfateDocument(context: Context) : Fragment(R.layout.fragment_afate_doc) {
     lateinit var mainactivity : MainActivity
     var myContext = context
+    lateinit var firebaseStorage: FirebaseStorage
+    lateinit var storageReference: StorageReference
+    lateinit var ref: StorageReference
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         mainactivity = activity as MainActivity
 
-        val documentPage= DocumentsFragment(myContext)
-
         val backButton = view.findViewById<ImageView>(R.id.backButton)
 
         backButton.setOnClickListener(){
 
-            val storageRef = FirebaseStorage.getInstance().getReference("Documents")
-            val islandRef = storageRef.child("AFATE/afati1.txt")
+            download()
 
-            val localFile = File.createTempFile("afati1", "txt")
+            mainactivity.setCurrentFragment(DocumentsFragment(myContext))
 
-            islandRef.getFile(localFile).addOnSuccessListener {
-                Toast.makeText(myContext,"Download complete", Toast.LENGTH_SHORT).show()
-            }.addOnFailureListener {
-                // Handle any errors
-            }
-
-            mainactivity.setCurrentFragment(documentPage)
         }
 
-
-
     }
+    fun download(){
+        storageReference = FirebaseStorage.getInstance().getReference()
+        ref = storageReference.child("Documents").child("AFATE").child("afati1.txt")
+
+
+        ref.getDownloadUrl().addOnSuccessListener(){
+            val url = it.toString()
+            downloadFile(myContext,"File",".txt", DIRECTORY_DOWNLOADS,url)
+        }.addOnFailureListener(){
+
+        }
+    }
+
+    fun downloadFile(
+        context: Context,
+        fileName: String,
+        fileExtension: String,
+        destinationDirectory: String?,
+        url: String?
+    ) {
+        val downloadmanager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val uri = Uri.parse(url)
+        val request = DownloadManager.Request(uri)
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalFilesDir(
+            context,
+            destinationDirectory,
+            fileName + fileExtension
+        )
+        downloadmanager.enqueue(request)
+    }
+
 }
